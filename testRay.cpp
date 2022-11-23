eae32549f4e949e4ef575242766386fc1b25b800
Rahul Khandelwal <rahulkhandelwal@Rahuls-MacBook-Air-2.local>
2022-11-22 23:52:13 -0500

    Basic Almost Ray Tracer and with OpenMP support

--- testRay.cpp
+++ testRay.cpp
@@ -1,16 +1,25 @@
 #include <png.h>
+#include "/usr/local/include/png.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <zlib.h>
 #include <string>
-#include <png++/png.hpp>
+//#include <png++/png.hpp>
+#include "/usr/local/include/png++/png.hpp"
 #include "vec2.h"
+#include <omp.h>
 
-#define RAYCOUNT 1000 // rays per source
-#define HIT_THRESHOLD 30000
+#define RAYCOUNT 10000 // rays per source
+#define HIT_THRESHOLD 1000
+
+#define L1_THRESHOLD 10
+#define L2_THRESHOLD 100
+#define L3_THRESHOLD 1000
+#define L4_THRESHOLD 10000
 
 const float incrRay = 0.1f;
-const float degradeFac = 0.99f;
+const float degradeFac = 0.98f;
+const float addFac = 100.f;
 
 struct ray 
 {
@@ -40,8 +49,8 @@ bool almostEqual(float a, float b) {
 }
 
 // updates the position and intensity of a ray while contributing its partial score to the new image
-void updateRay(Vec2f source, lightray *mRay, shifter *shifts, int cols) {
-  if (!(almostEqual(mRay->position.x, source.x) && almostEqual(mRay->position.y, source.y))) {
+void updateRay(lightray *source, lightray *mRay, shifter *shifts, int cols) {
+  if (!(almostEqual(mRay->position.x, source->position.x) && almostEqual(mRay->position.y, source->position.y))) {
     int accessRow = (int)mRay->position.y;
     int accessCol = (int)mRay->position.x;
 
@@ -52,11 +61,21 @@ void updateRay(Vec2f source, lightray *mRay, shifter *shifts, int cols) {
     accessPoint->greenshift += mRay->intensity;
   }
   // update position and intensity
-  mRay->intensity *= degradeFac;
+  //mRay->intensity *= degradeFac;
   Vec2f delta = mRay->velocity * incrRay;
   mRay->position.x += delta.x;
   mRay->position.y += delta.y;
+  float normDist = mRay->position.dist(source->position) / incrRay; // normalized distance
+  mRay->intensity = addFac * source->intensity / (normDist * normDist);
   return;
+}
+
+float computeContributionFactor(float score) {
+  if (score < L1_THRESHOLD) return 0.f;
+  else if (score < L2_THRESHOLD) return 0.25f;
+  else if (score < L3_THRESHOLD) return 0.5f;
+  else if (score < L2_THRESHOLD) return 0.75f;
+  else return 1.f;
 }
 
 // takes in <color.png> <traced.png> and produces an <output.png>
@@ -67,7 +86,11 @@ int main(int argc, char** argv) {
   } 
 
   const float defaultDistPixel = 1.0f;
-  Vec2f lightSource(141.3f, 52.84f);
+  Vec2f lightSourcePos(141.3f, 52.84f);
+  lightray lightSource;
+  lightSource.position = lightSourcePos;
+  lightSource.velocity = lightSourcePos;
+  lightSource.intensity = 255 + 255 + 255;
   png::rgb_pixel oneLight = png::rgb_pixel(255, 180, 180);
     
   png::image<png::rgb_pixel> colorImg(argv[1]);
@@ -96,19 +119,20 @@ int main(int argc, char** argv) {
   const Vec2f unitVecX(1.0f, 0.0f);
 
   // spawn rays from source
+  //#pragma omp parallel for
   for (int i = 0; i < RAYCOUNT; i++) {
     Vec2f curDir(unitVecX);
     double rotAmt = (double)i * (360.0 / RAYCOUNT);
     curDir.rotate(rotAmt);
     lightray mRay;
-    mRay.position = lightSource;
+    mRay.position = lightSourcePos;
     mRay.velocity = curDir;
     mRay.intensity = oneLight.red + oneLight.blue + oneLight.green;
 
     while (rayInBox(tl, br, mRay)) {
       if (tracedImg[(int)mRay.position.y][(int)mRay.position.x] == 0) break;
       else {
-        updateRay(lightSource, &mRay, (shifter*)shifts, cols); // update the ray
+        updateRay(&lightSource, &mRay, (shifter*)shifts, cols); // update the ray
       }
     }
   }
@@ -116,9 +140,9 @@ int main(int argc, char** argv) {
   // apply source light to image
   for (int r = 0; r < rows; r++) {
     for (int c = 0; c < cols; c++) {
-      int contributionRed = (oneLight.red - colorImg[r][c].red) * std::min(1.0f, shifts[r][c].redshift / HIT_THRESHOLD);
-      int contributionGreen = (oneLight.green - colorImg[r][c].green) * std::min(1.0f, shifts[r][c].greenshift / HIT_THRESHOLD);
-      int contributionBlue = (oneLight.blue - colorImg[r][c].blue) * std::min(1.0f, shifts[r][c].blueshift / HIT_THRESHOLD);
+      int contributionRed = (oneLight.red - colorImg[r][c].red) * computeContributionFactor(shifts[r][c].redshift); //std::min(1.0f, shifts[r][c].redshift / HIT_THRESHOLD);
+      int contributionGreen = (oneLight.green - colorImg[r][c].green) * computeContributionFactor(shifts[r][c].greenshift); //std::min(1.0f, shifts[r][c].greenshift / HIT_THRESHOLD);
+      int contributionBlue = (oneLight.blue - colorImg[r][c].blue) * computeContributionFactor(shifts[r][c].blueshift); //std::min(1.0f, shifts[r][c].blueshift / HIT_THRESHOLD);
 
       int newRed = std::min(255, colorImg[r][c].red + contributionRed);
       int newBlue = std::min(255, colorImg[r][c].blue + contributionBlue);
@@ -129,7 +153,7 @@ int main(int argc, char** argv) {
     }
   }
 
-
+  printf("score at up: %f\n", shifts[0][141].redshift);
   colorImg.write("output.png"); // finished product
   return 0;
 }

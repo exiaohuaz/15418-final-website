2d080e121a7fdb5a77dc4dff4a379490cb00ae70
Emily Zhang <exz@fb.com>
2022-11-27 16:31:15 -0500

    added serial convolution

---
+++ convolution.cpp
@@ -0,0 +1,43 @@
+#include <png.h>
+#include "/usr/local/include/png.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <zlib.h>
+#include "/usr/local/include/png++/png.hpp"
+
+#include "timing.h"
+
+typedef std::pair<int,int> P;
+P indices[9] = {P(-1, -1), P(-1, 0), P(-1, 1), P(0, -1),  P(0, 0),  P(0, 1), P(1, -1),  P(1, 0),  P(1, 1)};
+int mask[3][3] = {
+                {-1, -1, -1},
+                {-1, 8, -1},
+                {-1, -1, -1}
+                };
+
+int main(int argc, char** argv) {
+    png::image<png::gray_pixel> img(argv[1]);
+    int rows = img.get_height();
+    int cols = img.get_width();
+    printf("rows: %d  cols: %d\n", rows, cols);
+    png::image<png::gray_pixel> output(cols, rows);
+
+
+    Timer totalSimulationTimer;
+
+    for(int i = 1; i<rows-1; i++){ //edges arent computable
+        for(int j = 1; j<cols-1; j++){
+            int sum = 0;
+            for(int loc = 0; loc < sizeof(indices)/sizeof(P); loc++){
+                P diff = indices[loc];
+                sum += img[i+diff.first][j+diff.second] * mask[1+diff.first][1+diff.second];
+            }
+            output[i][j] = sum<=120?0:255;
+        }
+    }
+
+    double totalSimulationTime = totalSimulationTimer.elapsed();
+    printf("total simulation time: %.6fs\n", totalSimulationTime);
+
+    output.write("out/output.png");
+}

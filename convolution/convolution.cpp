b11e4036b51d3b152c1c803fe3c5fdea5e1e1e07
Emily Zhang <exz@fb.com>
2022-11-29 19:45:34 -0500

    still can't fix ispc lol

--- convolution/convolution.cpp
+++ convolution/convolution.cpp
@@ -5,7 +5,7 @@
 #include <zlib.h>
 #include <assert.h>
 #include "/usr/local/include/png++/png.hpp"
-
+#include "/usr/local/opt/libomp/include/omp.h"
 #include "timing.h"
 #include "CycleTimer.h"
 #include "convolution_ispc.h"
@@ -18,10 +18,37 @@ int mask[3][3] = {
                 {-1, -1, -1}
                 };
 
-void verify(png::image<png::gray_pixel> output, png::image<png::gray_pixel> output_parallel){
+void verify(int rows, int cols, png::image<png::gray_pixel> output, png::image<png::gray_pixel> output_parallel){
     for(int i = 0; i<rows; i++){
         for(int j = 0; j<rows; j++){
             assert(output[i][j] == output_parallel[i][j]);
+        }
+    }
+}
+
+void convolution_sequential(int rows, int cols, png::image<png::gray_pixel> &img, png::image<png::gray_pixel> &output){
+    for(int i = 1; i<rows-1; i++){ //edges arent computable
+        for(int j = 1; j<cols-1; j++){
+            int sum = 0;
+            for(int loc = 0; loc < sizeof(indices)/sizeof(P); loc++){
+                P diff = indices[loc];
+                sum += img[i+diff.first][j+diff.second] * mask[1+diff.first][1+diff.second];
+            }
+            output[i][j] = sum<=120?255:0;
+        }
+    }
+}
+
+void convolution_openmp(int rows, int cols, png::image<png::gray_pixel> &img, png::image<png::gray_pixel> &output){
+    #pragma omp parallel for collapse(2)
+    for(int i = 1; i<rows-1; i++){ //edges arent computable
+        for(int j = 1; j<cols-1; j++){
+            int sum = 0;
+            for(int loc = 0; loc < sizeof(indices)/sizeof(P); loc++){
+                P diff = indices[loc];
+                sum += img[i+diff.first][j+diff.second] * mask[1+diff.first][1+diff.second];
+            }
+            output[i][j] = sum<=120?255:0;
         }
     }
 }
@@ -34,29 +61,27 @@ int main(int argc, char** argv) {
     png::image<png::gray_pixel> output(cols, rows);
 
 
-    Timer totalSimulationTimer;
-
-    for(int i = 1; i<rows-1; i++){ //edges arent computable
-        for(int j = 1; j<cols-1; j++){
-            int sum = 0;
-            for(int loc = 0; loc < sizeof(indices)/sizeof(P); loc++){
-                P diff = indices[loc];
-                sum += img[i+diff.first][j+diff.second] * mask[1+diff.first][1+diff.second];
-            }
-            output[i][j] = sum<=120?255:0;
-        }
-    }
-
-    double totalSimulationTime = totalSimulationTimer.elapsed();
-    printf("total sequential simulation time: %.6fs\n", totalSimulationTime);
+    double startTime = CycleTimer::currentSeconds();
+    convolution_sequential(rows, cols, img, output);
+    double endTime = CycleTimer::currentSeconds();
+    printf("total sequential simulation time: %.6fs\n", endTime - startTime);
     output.write("out/output.png");
 
     
-    //this stuff is new and doesn't work
-    png::image<png::gray_pixel> output_ispc(cols, rows);
-    double startTime = CycleTimer::currentSeconds();
-    ispc::convolution_ispc(rows, cols, img, output_ispc);
-    double endTime = CycleTimer::currentSeconds();
-    verify(output, output_ispc);
-    printf("total ispc simulation time: %.6fs\n", endTime - startTime);
+    // this stuff is new and doesn't work
+    // png::image<png::gray_pixel> output_ispc(cols, rows);
+    // startTime = CycleTimer::currentSeconds();
+    // int total_elems = rows*cols;
+    // ispc::convolution_ispc(total_elems, img, output_ispc);
+    // endTime = CycleTimer::currentSeconds();
+    // verify(rows, cols, output, output_ispc);
+    // printf("total ispc simulation time: %.6fs\n", endTime - startTime);
+
+
+    png::image<png::gray_pixel> output_openmp(cols, rows);
+    startTime = CycleTimer::currentSeconds();
+    convolution_openmp(rows, cols, img, output_openmp);
+    endTime = CycleTimer::currentSeconds();
+    verify(rows, cols, output, output_openmp);
+    printf("total openmp simulation time: %.6fs\n", endTime - startTime);
 }

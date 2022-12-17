efe11677552ba4127c8191b9c591f435e6a2aaf2
Rahul Khandelwal <rahulkhandelwal@rahuls-air-2.wifi.local.cmu.edu>
2022-12-17 18:42:59 -0500

    livedemo app working

--- livedemo/demo.py
+++ livedemo/demo.py
@@ -1,14 +1,51 @@
 from cmu_112_graphics import *
-import math
 from PIL import Image
 import subprocess
 
+def incrementLast(n):
+    lights = open("lights.txt", "r")
+    allSources = lights.readlines()
+    print(allSources)
+    lights.close()
+    lights = open("lights.txt", 'w')
+    lastIndex = len(allSources) - 1
+    for line in range(lastIndex):
+        lights.write(allSources[line])
+    last = allSources[lastIndex]
+    elems = last.split(" ")
+    lastElemIndex = len(elems) - 1
+    newSource = ""
+    for elemIdx in range(lastElemIndex):
+        newSource += (elems[elemIdx] + " ")
+    if (n == 0): prevIntensity = 10
+    else: prevIntensity = float(elems[lastElemIndex])
+    newSource += str(prevIntensity * 1.1)
+    lights.write(newSource)
+    lights.close()
+
+def cpf():
+    key = input("Reset Images?: ")
+    if (not(key == "yes")): return
+    subprocess.call("rm -rf demodata", shell=True)
+    subprocess.call("mkdir demodata", shell=True)
+    frames = int(subprocess.check_output("ls ../data | wc -l", shell=True).decode().strip())
+    
+    for i in range(frames):
+        imN = "frame" + str(i * 20) + ".png"
+        imgSrc = f"../data/{imN}"
+        txtF = "lights.txt" 
+        outLoc = f"demodata/{imN}"
+        incrementLast(i)
+        subprocess.call(f"make run SRC={imgSrc} TXT={txtF} OUT={outLoc}", shell=True)
+        print("done " + str(i))
+
+    return
 
 def appStarted(app):
-    app.imx = 2560/2
-    app.imy = 1440/2
-    app.imgDir = subprocess.check_output("pwd", shell=True).decode().strip() + "/../data"
+    app.imgDir = "demodata"
     app.fImg = Image.open(app.imgDir + "/frame0.png")
+    app.imx = app.fImg.width / 2
+    app.imy = app.fImg.height / 2
     app.imgCount = 0
     app.lastFrameNum = 2760
     app.paused = False
@@ -20,7 +57,7 @@ def keyPressed(app, event):
     if event.key == 'r':
         appStarted(app)
     elif event.key == 'p':
-        app.paused = True
+        app.paused = not app.paused
 
 def timerFired(app):
     if not app.paused:
@@ -28,10 +65,10 @@ def timerFired(app):
         if (app.imgCount > app.lastFrameNum):
             app.imgCount = 0
         app.fImg = Image.open(app.imgDir + f"/frame{app.imgCount}.png")
-    print("framestep")
 
 def redrawAll(app, canvas):
     canvas.create_image(app.imx, app.imy, image=ImageTk.PhotoImage(app.fImg))
     return
 
-runApp(width=2560, height=1440)
+#cpf()
+runApp(width=1066, height=600)
